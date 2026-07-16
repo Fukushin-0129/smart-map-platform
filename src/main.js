fb4c67a3ead8f2b102a91b039707098f27dfcb30
fukushin <101027858+Fukushin-0129@users.noreply.github.com>
2026-07-16 15:41:23 +0900

    Initialize map-based flyer place picker

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import { installMapCapture } from './modules/mapCapture.js';
 import { initializeApp } from './modules/ui.js';
+import { initializeFlyerMapPicker } from './modules/flyerMapPicker.js';
 
+installMapCapture();
 initializeApp();
+initializeFlyerMapPicker();
