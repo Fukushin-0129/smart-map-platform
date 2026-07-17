bb414dabef8ece9870368e5776d46aa5d3cec190
fukushin <101027858+Fukushin-0129@users.noreply.github.com>
2026-07-17 10:55:22 +0900

    Initialize flyer spreadsheet round-trip controls

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { initializeApp } from './modules/ui.js';
 import { initializeFlyerMapPicker } from './modules/flyerMapPicker.js';
 import { initializeFlyerContinuousRegistration } from './modules/flyerContinuousRegistration.js';
 import { initializeFlyerDelete } from './modules/flyerDelete.js';
+import { initializeFlyerSpreadsheetRoundTrip } from './modules/flyerSpreadsheetRoundTrip.js';
 
 installMapCapture();
 initializeApp();
 initializeFlyerMapPicker();
 initializeFlyerContinuousRegistration();
 initializeFlyerDelete();
+initializeFlyerSpreadsheetRoundTrip();
